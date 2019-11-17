54f5bf513d415f31208c1e443a3dd1f09a799553
Kattenelvis <officialkattenelvis@gmail.com>
2019-11-17 22:36:23 +0100

    aaa

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 /// <reference path="./secand.ts" />
+/// <reference path="./display.ts" />
 
 namespace WorldSimulator
 {
@@ -6,35 +7,21 @@ namespace WorldSimulator
     {
         public static Main():void
         {
-            const Canvas:HTMLElement|null = document.getElementById("canvas")
-            const c = Canvas.getContext("2d")
-
-            const graph = new Graph();
-            graph.Render(c)
+            const graph = new Display.Graph();
+            graph.addNode(50)
+            graph.addNode(60)
+            graph.addNode(110)
+            graph.Render()
 
         }
     }
 
-    class Graph
+    class Data
     {
-        public Nodes = {n1:90, n2:50, n3:200, n4:230}
-
-        public Render(ctx:any): void
-        {
-            ctx.beginPath()
-            ctx.moveTo(1,this.Nodes.n1)
-
-            let point = 1
-            for (const node in this.Nodes) {
-                ctx.lineTo(point*15, this.Nodes[node])
-                point++
-            }
-            ctx.stroke()           
-        }
+        
     }
 }
 
 
-
 WorldSimulator.worldSimulator.Main();
-secand.sayhi();
+// secand.sayhi();
--- out.js
+++ out.js
@@ -1,4 +1,30 @@
 "use strict";
+var WorldSimulator;
+(function (WorldSimulator) {
+    var Display;
+    (function (Display) {
+        class Graph {
+            constructor() {
+                this.Nodes = [90, 80, 250, 330];
+            }
+            addNode(node) {
+                this.Nodes.push(node);
+            }
+            Render() {
+                const ctx = window.ctx;
+                ctx.beginPath();
+                ctx.moveTo(1, this.Nodes[0]);
+                let point = 1;
+                for (const node in this.Nodes) {
+                    ctx.lineTo(point * 15, this.Nodes[node]);
+                    point++;
+                }
+                ctx.stroke();
+            }
+        }
+        Display.Graph = Graph;
+    })(Display = WorldSimulator.Display || (WorldSimulator.Display = {}));
+})(WorldSimulator || (WorldSimulator = {}));
 var secand;
 (function (secand) {
     function sayhi() {
@@ -10,32 +36,21 @@ var secand;
 const howdy = hello("hiiii");
 console.log(howdy("I am a bbgummy bear"));*/
 /// <reference path="./secand.ts" />
+/// <reference path="./display.ts" />
 var WorldSimulator;
 (function (WorldSimulator) {
     class worldSimulator {
         static Main() {
-            const Canvas = document.getElementById("canvas");
-            const c = Canvas.getContext("2d");
-            const graph = new Graph();
-            graph.Render(c);
+            const graph = new WorldSimulator.Display.Graph();
+            graph.addNode(50);
+            graph.addNode(60);
+            graph.addNode(110);
+            graph.Render();
         }
     }
     WorldSimulator.worldSimulator = worldSimulator;
-    class Graph {
-        constructor() {
-            this.Nodes = { n1: 90, n2: 50, n3: 200, n4: 230 };
-        }
-        Render(ctx) {
-            ctx.beginPath();
-            ctx.moveTo(1, this.Nodes.n1);
-            let point = 1;
-            for (const node in this.Nodes) {
-                ctx.lineTo(point * 15, this.Nodes[node]);
-                point++;
-            }
-            ctx.stroke();
-        }
+    class Data {
     }
 })(WorldSimulator || (WorldSimulator = {}));
 WorldSimulator.worldSimulator.Main();
-secand.sayhi();
+// secand.sayhi();
